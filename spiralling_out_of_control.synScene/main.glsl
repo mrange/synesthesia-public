7d4f59b268f6a14a7d87dfd38f1b7fda89e6058b
mrange <marten_range@hotmail.com>
2025-09-17 17:16:28 +0200

    Tweaks

---
+++ spiralling_out_of_control.synScene/main.glsl
@@ -0,0 +1,91 @@
+// -----------------------------------------------------------------------------
+// This file is released under CC0 1.0 Universal (Public Domain Dedication).
+// To the extent possible under law, Mårten Rånge has waived all copyright
+// and related or neighboring rights to this work.
+// See <https://creativecommons.org/publicdomain/zero/1.0/> for details.
+
+// When I wrote this code, only god and
+//  I knew how it worked.
+//  Now, only god knows it!
+
+const vec2
+  spiral_col_xy   = vec2(3,2)
+, spiral_col_zw   = vec2(1,0)
+, spiral_col_twist= vec2(4,1)
+, spiral_dim      = vec2(.9,.5)
+, border_fade     = vec2(1.5,.5)
+, border_glow     = vec2(.5,1.5)
+, border_apply    = vec2(1,0)
+;
+
+const vec3
+  flash_col = vec3(3,2,1)/3.
+;
+
+#ifdef KODELIFE
+const float
+  double_spiral=1.
+, spiral_twist =.4
+, spiral_rot   =.3
+, color_rot    =.0
+;
+
+#endif
+
+float g(vec4 p,float s) {
+  return abs(dot(sin(p*=s),cos(p.zxwy))-1.)/s;
+}
+
+mat2 rot(float a) {
+  float c=cos(a),s=sin(a);
+  return mat2(c,s,-s,c);
+}
+
+vec4 renderMain() {
+  vec2
+    S=spiral_col_twist
+  , Q=spiral_dim
+  ;
+  vec3
+    I=normalize(vec3(_uvc,1))
+  , O=vec3(0)
+  ;
+  float
+      z=0.
+    , Y
+    , D
+#ifdef KODELIFE
+    , T=TIME
+#else
+    , T=path_speed
+#endif
+    , d=double_spiral
+    , W=spiral_twist
+    , R=spiral_rot
+    , C=color_rot
+    , L=length(-1.+2.*_uv)
+    ;
+  vec4
+      p
+    , X
+    , U=vec4(spiral_col_xy,spiral_col_zw)
+    ;
+  for(
+      int i=0
+    ; i<77
+    ; ++i
+    ) {
+    p=z*I.xyzy;
+    p.z+=T;
+    D=g(p,23.) + g(p,11.) + g(p,7.);
+    p.xy*=rot(R+W*p.z);
+    Y=Q.x+Q.y*sin(.5*p.z);
+    X=d>.5?abs(p):p;
+    X/=Y;
+    z+=D=.7*abs(Y*abs(X.x-round(max(X.x, 1.)))+D/9.-.1)+1e-3;
+    D*=1.+dot(p.xy,p.xy)/4.;
+    p=1.+sin(dot(S,p.xy)+(p.x<0.?C+.25+U.yzwy : U.zyxz-.25-C));
+    O+=p.w/D*p.xyz;
+  }
+  return vec4(mix(1.,smoothstep(border_fade.x,border_fade.y, L), border_apply.x)*tanh(border_apply.y*2.*smoothstep(border_glow.x,border_glow.y, L)+(O+z*z*z*6.*flash_col)/9e3),1);
+}
