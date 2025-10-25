cd5498202c5170db68ccf3648a4ac37c76fa4f9d
mrange <marten_range@hotmail.com>
2025-10-25 15:33:49 +0200

    GG

--- mandel_madness.synScene/main.glsl
+++ mandel_madness.synScene/main.glsl
@@ -19,6 +19,16 @@ vec4 alphaBlend(vec4 back, vec4 front) {
   return w > 0.0 ? vec4(xyz, w) : vec4(0.0);
 }
 
+
+float beat() {
+#ifdef KODELIFE
+  return pow(1.-fract(TIME),2.);
+#else
+  return dot(pow(vec2(syn_BassLevel,syn_BassHits), bass_pow), bass_mix);
+#endif
+}
+
+
 float freq(float x) {
 #ifdef KODELIFE
   return 0.;
@@ -41,62 +51,65 @@ vec4 renderMain() {
 #endif
   , i
   , l
+  , D
+  , L
+  , B=beat()
+  , re2
+  , im2
+  , reim
+  , mag2=0.
+  , shd
   ;
   vec2
     q   = _uv
   , p   = 2.*_uvc
-  , op  = p
   , tsz = vec2(textureSize(syn_Media,0))
   , tz  = vec2(tsz.y/tsz.x, 1)
   , c = initial_c
   , z
   ;
-
-  rot(p,rotation_speed+dot(twist,vec2(length(p),dot(p,p))));
-  p = zoom_center + p*zoom_factor.x*pow(zoom_factor.y, 0.5+0.5*cos(0.3*sqrt(2.0)*tm));
+  D=dot(p,p);
+  L=sqrt(D);
+  p*=mix(1.,1./(1.+dot(zoom_beat, vec2(L,D))),B);
+  rot(p,rotation_speed+dot(B*twist,vec2(L,D)));
+  p = zoom_center + zoom_factor.x*pow(zoom_factor.y, 0.5+0.5*cos(0.3*sqrt(2.0)*tm))*p;
 
   vec3
-    ss = mix(vec3(0.2, 0.2, 0.5), vec3(0.2,-0.2,1.0), 2.2 + 1.25*sin(tm/2.0))
-  , col = vec3(0)
+    col = vec3(0)
   ;
 
   z=p;
   rot(c, rotation_c.x*sin(rotation_c.y*tm));
 
   for(i=0.; i<maxIterF; ++i) {
-    float
-      re2  = z.x*z.x
-    , im2  = z.y*z.y
-    , reim = z.x*z.y
-    ;
-    if ((re2+im2)>20.) break;
+    re2  = z.x*z.x;
+    im2  = z.y*z.y;
+    reim = z.x*z.y;
+    mag2 = re2+im2;
 
     z = vec2(re2 - im2, 2.*reim) + c;
 
-    float shade = smoothstep(maxIterF, 0., i);
-    shade /= re2+im2+0.01;
-    col+=shade*3e-3*(1.+sin(vec3(4.*z, 0.2*i)+vec3(0,color_control)))/abs(z.y+.1*(freq(2.*z.x)-.5));
-//    bg+=shade*3e-3*(1.+sin(vec3(4.*z, 0.2*i)+vec3(0,1,2)))/abs(z.y+sin(TIME+10.*z.x)-.2);
-//    bg+=shade*3e-3*(1.+sin(vec3(5.*z, 0.1*i)+vec3(2,1,0)))/abs(z.y*z.y-.2);
-//    bg+=shade*3e-3*(1.+sin(vec3(5.*z, 0.1*i)+vec3(2,1,0)))/abs(z.x*z.x-.3);
-//  bg+=shade*3e-3*(1.+sin(vec3(6.*z, 0.1*i)+vec3(1,2,0)))/abs(z.x*z.y-.3);
+    shd = smoothstep(maxIterF, 0., i);
+    shd /= mag2+.01;
+    col+=shd*3e-3/abs(z.y+.1*(freq(2.*z.x)-.5))*(1.+sin(vec3(4.*z, 0.2*i)+vec3(0,color_control)));
+    if (mag2>20.) break;
   }
   l = i - log2(log2(dot(z,z)));
 
   col /= color_divider;
   if (!isnan(l)) {
-    col += (0.5 + 0.5*cos(l*.5 + 3.+ vec3(0,2.*color_control)))/(1.+l*l);
+    col += 1./(1.+l*l)*(0.5 + 0.5*cos(l*.5 + 3.+ vec3(0,2.*color_control)));
   }
   col = tanh(col);
   col = mix(col, 1.-col, invert_color);
 
   col = max(col,0.);
   vec4 pcol = texture(syn_FinalPass, q);
-  col.xyz = mix(col.xyz, pcol.xyz, mix(motion_blur_intensity.x, motion_blur_intensity.y, smoothstep(motion_blur_limits.x, motion_blur_limits.y, length(p))));
+  col.xyz = mix(col.xyz, pcol.xyz, mix(motion_blur_intensity.x, motion_blur_intensity.y, smoothstep(motion_blur_limits.x, motion_blur_limits.y, L)));
 
 #ifdef KODELIFE
 #else
-  vec4 mcol=_loadMedia(media_warp.x*normalize(p)*freq(media_warp.y*length(p)));
+  vec4 mcol=_loadMedia(media_warp.x*normalize(p)*freq(media_warp.y*L));
   col=mix(col,mcol.xyz,mcol.w*media_opacity*media_multiplier);
 #endif
   
