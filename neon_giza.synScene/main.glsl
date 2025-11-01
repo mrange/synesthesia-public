5519ab3e1591df4e94aea2f58a94899953ea2db3
mrange <marten_range@hotmail.com>
2025-11-01 12:59:18 +0100

    Tweaks

--- neon_giza.synScene/main.glsl
+++ neon_giza.synScene/main.glsl
@@ -1,9 +1,9 @@
 const float
   TAU                 = 2.*PI
-, MAX_RAY_LENGTH_HI   = 48.0
-, TOLERANCE_HI        = 0.0001
+, MAX_RAY_LENGTH_HI   = 60.0
+, TOLERANCE_HI        = 1e-4
 , MAX_RAY_MARCHES_HI  = 70.
-, NORM_OFF            = 0.001
+, NORM_OFF            = 5e-3
 ;
 
 #define ROT(a)              mat2(cos(a), sin(a), -sin(a), cos(a))
@@ -19,23 +19,69 @@ vec3 hsv2rgb(vec3 c) {
 //  Macro version of above to enable compile-time constants
 #define HSV2RGB(c)  (c.z * mix(hsv2rgb_K.xxx, clamp(abs(fract(c.xxx + hsv2rgb_K.xyz) * 6.0 - hsv2rgb_K.www) - hsv2rgb_K.xxx, 0.0, 1.0), c.y))
 
-//#define PATHA (0.33*vec2(0.1147, 0.2093))
-//#define PATHB (0.33*vec2(13.0, 3.0))
+#define ROTY(a)               \
+  mat3(                       \
+    +cos(a) , 0.0 , +sin(a)   \
+  , 0.0     , 1.0 , 0.0       \
+  , -sin(a) , 0.0 , +cos(a)   \
+  )
+
+#define ROTZ(a)               \
+  mat3(                       \
+    +cos(a) , +sin(a) , 0.0   \
+  , -sin(a) , +cos(a) , 0.0   \
+  , 0.0     , 0.0     , 1.0   \
+  )
+
+#define ROTX(a)               \
+  mat3(                       \
+    1.0 , 0.0     , 0.0       \
+  , 0.0 , +cos(a) , +sin(a)   \
+  , 0.0 , -sin(a) , +cos(a)   \
+  )
+
+
+const float 
+  PERIOD=300.
+;
+  
 const vec2
-  PATHA = (TAU/300.*vec2(3,2))
+  PATHA = (TAU/PERIOD*vec2(3,2))
 , PATHB = (vec2(2, 3))
 ;
   
+const mat2
+  R45 = ROT(-PI/4.)
+;
+
+const mat3
+  rotx = ROTX(radians(-51.8))
+, rr0  = rotx
+, rr1  = rr0*ROTY(PI/2.0)
+;
+
+const vec3
+  n0   = normalize(vec3(.0, 0.0, 1.0))*rr0
+, c0   = vec3(0.0)
+, n1   = normalize(vec3(.0, 0.0, 1.0))*rr1
+, c1   = vec3(0.0)
+;
+
+const vec4
+  dim0 = vec4(n0, -dot(c0, n0))
+, dim1 = vec4(n1, -dot(c1, n1))
+;
+
 vec3 path(float z) {
   return vec3(sin(z*PATHA)*PATHB, z);
 }
 
 vec3 dpath(float z) {
-  return vec3(PATHA*PATHB*cos(PATHA*z), 1.0);
+  return vec3(PATHA*PATHB*cos(PATHA*z), 1);
 }
 
 vec3 ddpath(float z) {
-  return vec3(-PATHA*PATHA*PATHB*sin(PATHA*z), 0.0);
+  return vec3(-PATHA*PATHA*PATHB*sin(PATHA*z), 0);
 }
 
 // License: Unknown, author: Claude Brezinski, found: https://mathr.co.uk/blog/2017-09-06_approximating_hyperbolic_tangent.html
@@ -97,7 +143,7 @@ float flatTorus( vec3 p, vec2 t) {
   return length4(q)-t.y;
 }
 
-// License: MIT, author: Inigo Quilez, found: https://iquilezles.org/articles/distfunctions2d/
+// License: MIT, author: Inigo Quilez, found: httfps://iquilezles.org/articles/distfunctions2d/
 float cappedTorus(vec3 p, vec2 sc, vec2 t) {
   float
     ra = t.x
@@ -116,35 +162,15 @@ float arc(vec2 p, vec2 sc, float ra, float rb) {
                                 abs(length(p)-ra)) - rb;
 }
 
-#define ROTY(a)               \
-  mat3(                       \
-    +cos(a) , 0.0 , +sin(a) \
-  , 0.0     , 1.0 , 0.0     \
-  , -sin(a) , 0.0 , +cos(a) \
-  )
-
-#define ROTZ(a)               \
-  mat3(                       \
-    +cos(a) , +sin(a) , 0.0   \
-  , -sin(a) , +cos(a) , 0.0   \
-  , 0.0     , 0.0     , 1.0   \
-  )
-
-#define ROTX(a)               \
-  mat3(                       \
-    1.0 , 0.0     , 0.0       \
-  , 0.0 , +cos(a) , +sin(a)   \
-  , 0.0 , -sin(a) , +cos(a)   \
-  )
-
 const float
   hoff      = 0.725;
 const mat3
   roty      = ROTY(radians(10.0))
+, rotline   = transpose(roty)*ROTX(0.027)
 ;
 const vec3
-  sunDir     = normalize(vec3(0.0,-0.01, 1.0))*roty
-, lightPos   = vec3(0.0, -60.0, -200.0)*roty
+  sunDir     = normalize(vec3(0,-0.01, 1))*roty
+, lightPos   = vec3(0, -60, -200)*roty
 , sunColor   = HSV2RGB(vec3(hoff+0.0, 0.9, 0.0005))
 , topColor   = HSV2RGB(vec3(hoff+0.0, 0.9, 0.0001))
 , glowColor0 = HSV2RGB(vec3(hoff+0.0, 0.9, 0.0001))
@@ -196,7 +222,7 @@ vec3 sky(vec3 ro, vec3 rd) {
   vec3
     col = sunColor/(1.0+1e-5 - dot(sunDir, rd))
   ;
-  col += 100.0*glowColor0/sqrt(hd);
+  col += 100.0*glowColor0*inversesqrt(hd);
   col += glowColor2/(hd);
   return col;
 }
@@ -217,12 +243,12 @@ vec3 glow(vec3 ro, vec3 rd, float beat) {
     lightDir  = normalize(lightPos-ro)
   ;
   float
-    g3        = 1.0+0.00001 - dot(lightDir, rd)
+    g3        = 1.+1e-5 - dot(lightDir, rd)
   ;
 
   vec3
     col = 8.*glowColor0/(g3)
-  , rrd = rd*transpose(roty)*ROTX(0.027)
+  , rrd = rd*rotline
   ;
 
   if (bi != vec2(-1)) {
@@ -239,26 +265,33 @@ vec3 glow(vec3 ro, vec3 rd, float beat) {
 }
 
 vec3 side(vec3 col, vec3 ro, vec3 rd, float t, vec4 dim, vec3 c) {
-  vec3 n = dim.xyz;
+  const vec2 tri =vec2(485, sqrt(3.0)*356.0);
+  vec3 
+    n = dim.xyz
+  , p = ro + rd*t
+  , r = reflect(rd, n)
+  , ldiff = p - lightPos
+  , ld = normalize(ldiff)
+  , rcol0 = sky(p, r)
+  ;
 
-  vec3 p = ro + rd*t;
-
-  vec3 r = reflect(rd, n);
-  vec3 ldiff = p - lightPos;
-  vec3 ld = normalize(ldiff);
-  vec3 rcol0 = sky(p, r);
   float dcol = max(dot(ld, n), 0.0);
   dcol *= dcol;
-  vec2 pp = planeCoord(p, c, vec3(0.0, 1.0, 0.0), dim);
-  vec2 p0 = pp;
-  vec2 p1 = pp;
-  const vec2 tri =vec2(485, sqrt(3.0)*356.0);
-  float d0 = triIso(p0, tri);
-  float d1 = triIso(p1, 0.11*tri);
-  float d = d0;
-  vec3 bcol = col;
-  float hf = smoothstep(-600.0, -400.0, p.y);
-  vec3 pcol = 3.0*diffColor*dcol;
+  vec2 
+    pp = planeCoord(p, c, vec3(0, 1, 0), dim)
+  , p0 = pp
+  , p1 = pp
+  ;
+  float 
+    d0 = triIso(p0, tri)
+  , d1 = triIso(p1, 0.11*tri)
+  , d = d0
+  , hf = smoothstep(-600.0, -400.0, p.y)
+  ;
+  vec3 
+    bcol = col
+  , pcol = 3.0*diffColor*dcol
+  ;
   pcol += rcol0;
   pcol = mix(clamp(col, 0.0, 0.1), pcol, hf);
   col = mix(col, pcol, smoothstep(9., 0., d));
@@ -267,23 +300,6 @@ vec3 side(vec3 col, vec3 ro, vec3 rd, float t, vec4 dim, vec3 c) {
 }
 
 vec3 pyramid(vec3 col, vec3 ro, vec3 rd) {
-  const mat3
-    rotx = ROTX(radians(-51.8))
-  , rr0  = rotx
-  , rr1  = rr0*ROTY(PI/2.0)
-  ;
-
-  const vec3
-    n0   = normalize(vec3(.0, 0.0, 1.0))*rr0
-  , c0   = vec3(0.0)
-  , n1   = normalize(vec3(.0, 0.0, 1.0))*rr1
-  , c1   = vec3(0.0)
-  ;
-
-  const vec4
-    dim0 = vec4(n0, -dot(c0, n0))
-  , dim1 = vec4(n1, -dot(c1, n1))
-  ;
   float
     t0  = rayPlane(ro, rd , dim0)
   , t1  = rayPlane(ro, rd , dim1)
@@ -300,7 +316,12 @@ vec3 pyramid(vec3 col, vec3 ro, vec3 rd) {
   return col;
 }
 
-float g_gd;
+vec2 g_gd;
+
+// License: Unknown, author: Unknown, found: don't remember
+float hash(float co) {
+  return fract(sin(co*12.9898) * 13758.5453);
+}
 
 void warpWorld(inout vec3 p){
   vec3
@@ -313,26 +334,18 @@ void warpWorld(inout vec3 p){
   rot(p.xy,-50.*ddwarp.x);
 }
 
-// License: Unknown, author: Unknown, found: don't remember
-float hash(float co) {
-  return fract(sin(co*12.9898) * 13758.5453);
-}
-
 vec2 dfArcs(vec3 p) {
-  const mat2
-    R = ROT(-PI/4.)
-  ;
   const vec2
     sc=SCA(PI/16.)
   ;
-
+  
   vec3
     p1 = p
   ;
   float
     n1 = round(.25*p1.z)
-  , h1 = hash(mod(n1+37.5,75.)-37.5)
-  , sh1 = -1.0+2.0*h1
+  , h1 = hash(mod(n1+.125*PERIOD,.25*PERIOD)-.125*PERIOD)
+  , sh1 = -1.+2.*h1
   ;
   p1.z-=4.*n1;
   vec3
@@ -340,12 +353,12 @@ vec2 dfArcs(vec3 p) {
     ;
   rot(p1.xy,.25*TIME*sh1);
   p1.xy = abs(p1.xy);
-  p1.xy *= R;
+  p1.xy *= R45;
   float
-    d1 = cappedTorus(p1, sc, 3.0*vec2(1.0, 0.075))
-  , d2 = flatTorus(p2, 3.0*vec2(1.0, 0.03))
+    d1 = cappedTorus(p1, sc, vec2(3, 0.225))
+  , d2 = flatTorus(p2, vec2(3, .09))
   ;
-  d2 = max(d2, -d1);
+  
   return vec2(d1, d2);
 }
 
@@ -359,18 +372,19 @@ float df(vec3 p, float t) {
     d0 = arc(p0.xy, SCA(PI/6.0), 6.0, 0.8)
   , d = d0
   ;
-  d = pmax(d, -(dd1.y-0.25), 0.125);
+  d = pmax(d, 0.25-dd1.y, 0.125);
   d = min(d, dd1.x);
   d = min(d, dd1.y);
   float gd = dd1.x;
-  t = max(t-MAX_RAY_LENGTH_HI*0.5, 0.0);
-  g_gd = min(g_gd, gd+t*t*1E-4);
+  if (gd<g_gd.x) {
+    g_gd=vec2(gd,t);
+  }
 
   return d;
 }
 
 float df(vec3 p) {
-  return df(p, 0.0);
+  return df(p,0.);
 }
 
 vec3 normal(vec3 pos) {
@@ -383,20 +397,19 @@ vec3 normal(vec3 pos) {
 }
 
 float rayMarch(vec3 ro, vec3 rd, float initt, out float iter) {
-  const float tol = TOLERANCE_HI;
   float
     t = initt
   , i
   ;
   for (i = 0.; i < MAX_RAY_MARCHES_HI; ++i) {
-    float d = df(ro + rd*t, t);
+    float d = df(ro + rd*t,t);
     if (d < TOLERANCE_HI || t > MAX_RAY_LENGTH_HI) {
       break;
     }
     t += d;
   }
   iter = i;
-  return t;
+  return min(t,MAX_RAY_LENGTH_HI);
 }
 
 
@@ -405,12 +418,9 @@ vec3 render0(vec3 ro, vec3 rd, float beat) {
     vec3 ro_ = vec3(0.0, 0.0, -2700.0)*roty
   ;
   const
-    mat3 rrd = ROTX(-0.2)*ROTY(0.)
+    mat3 rrd = ROTX(-0.2)
   ;
-  const float
-    rdd=  3.0
-  , mm = 4.0
-  ;
+
   rd *= rrd;
   ro = ro_;
 
@@ -421,30 +431,35 @@ vec3 render0(vec3 ro, vec3 rd, float beat) {
 
   col = pyramid(col, ro, rd);
   col += glowCol;
-  col = clamp(col, 0., 10.);
-
+  col=clamp(col,0.,4.);
   return col;
 }
 
-vec3 render1(vec3 ro, vec3 rd, vec2 sp) {
-  g_gd = 1E3;
+vec3 render1(vec3 ro, vec3 rd, vec2 sp, out float depth) {
+  g_gd = vec2(1E3);
   float
     B = beat()
   , iter
   , t = rayMarch(ro, rd, 0.0, iter)
-  , gd = g_gd
+  , hd   
   ;
+  vec2
+    gd=g_gd
+  ;
+  hd=smoothstep(0.,0.01,gd.x);
   vec3
-    ggcol = (glowCol1)/(max(gd, 1e-3))
-  , skyCol = render0(ro, rd, B)
-  , col = skyCol
+    ggcol   = glowCol1/(max(gd.x, 1e-3))
+  , skyCol  = render0(ro, rd, B)
+  , col     = vec3(0)
+  , rcol    = vec3(0.0)
   ;
+  ggcol=clamp(ggcol,0.,4.);
 
-  float tt = t/MAX_RAY_LENGTH_HI;
-  tt -= 0.33;
-  tt = clamp(tt, 0.0, 1.0);
-  float sfo = 1.-exp(-9.*tt*tt);
-
+  float 
+    tt = max(t/MAX_RAY_LENGTH_HI-.3,0.)
+  , sfo = 1.-exp(-9.*tt*tt)
+  ;
+  depth=t;
   if (t < MAX_RAY_LENGTH_HI) {
     vec3
       p = ro+rd*t
@@ -452,8 +467,8 @@ vec3 render1(vec3 ro, vec3 rd, vec2 sp) {
     , r = reflect(rd, n)
     ;
     float
-      fre0 = 1.0+dot(rd, n)
-    , fre = fre0
+      fre0 = 1.+dot(rd, n)
+    , fre  = fre0
     ;
     fre *= fre;
 
@@ -467,14 +482,18 @@ vec3 render1(vec3 ro, vec3 rd, vec2 sp) {
     warpWorld(wp);
     vec2 dd = dfArcs(wp);
 
-    vec3 rcol = vec3(0.0);
     float hit = min(dd.x, dd.y);
     if (hit > .05) {
-      g_gd = 1E3;
-      float riter;
-      float rt = rayMarch(p, r, 0.5,riter);
-      float rgd = g_gd;
-      vec3 rggcol = 0.5/(max(rgd*rgd, 1e-3))*(glowCol1);
+      g_gd = vec2(1E3);
+      float 
+        riter
+      , rt = rayMarch(p, r, 0.5,riter)
+      ;
+      vec2 
+        rgd=g_gd
+      ;
+      vec3 rggcol = 0.5/(max(rgd.x*rgd.x, 1e-3))*(glowCol1);
+      rggcol=clamp(rggcol,0.,4.);
       rcol = rggcol;
       rcol *= smoothstep(0.66, 0.1, tt);
       if (rt < MAX_RAY_LENGTH_HI) {
@@ -484,53 +503,69 @@ vec3 render1(vec3 ro, vec3 rd, vec2 sp) {
       }
     }
 
-    rcol += 4.0*smoothstep(0.,0.01,gd)/max(dd.x*dd.x, 0.01)*(diffColor+0.5)*glowCol1;
-    col = vec3(0);
+    rcol += 4.0*hd/max(dd.x*dd.x, 0.01)*(diffColor+0.5)*glowCol1;
     col += dif*dif*fo*1e5*sunColor*diffColor;
     col += fre*rcol;
   }
 
-  col = mix(col, skyCol, sfo);
-
   col += ggcol;
+  col = mix(
+      mix(col,col*.25*vec3(2,1,3),sfo)
+    , skyCol+hd*ggcol*smoothstep(MAX_RAY_LENGTH_HI, MAX_RAY_LENGTH_HI*.5, gd.y)
+    , sfo
+    );
 
-  vec3 rrd = rd*transpose(roty)*ROTX(0.027);
+
+  vec3 rrd = rd*rotline;
   float flash = dot(rrd, normalize(vec3(0.0, -0.2, -1.0)))+1.0005;
   col += (0.01*vec3(0.5, 0.25, 1.0))*smoothstep(.5, 1., B*B)/flash;
-
 
   return col;
 }
 
 
-vec3 effect(vec2 p, vec2 pp, vec2 q) {
-  const vec3
-    up = vec3(0, 1, 0)
+vec4 fpass0() {
+  vec2 
+    q = _uv
+  , pp=-1.+2.*q
+  , p = 2.*_uvc
   ;
 
   float
-    z = 5.0*(mod(TIME, 60.)-30.0)
+    z = mod(5.*TIME, PERIOD)-PERIOD*.5
+  , depth
   ;
-
-  vec2
-    np = p + 4.0/RENDERSIZE.y;
 
   vec3
     ro = path(z)
-  , ww = normalize(dpath(z))
+  , Z  = normalize(dpath(z))
   , dd = ddpath(z)
-  , uu = normalize(cross(up+10.*dd, ww))
-  , vv = cross(ww,uu)
-  , rd = normalize(-p.x*uu + p.y*vv + 2.*ww)
-  , col= render1(ro, rd, p)
+  , X  = normalize(cross(vec3(0, 1, 0)+10.*dd, Z))
+  , Y  = cross(Z,X)
+  , rd = normalize(-p.x*X + p.y*Y + 2.*Z)
+  , col= render1(ro, rd, p, depth)
   ;
 
-  col -= 0.025*vec3(2,3,1)*(.25+length(pp));
+  col -= .025*(.25+length(pp))*vec3(2,3,1);
   col *= smoothstep(1.5, 0.5, length(pp));
-  col = clamp(col, 0.0, 4.0);
-  vec4
-    pcol=texture(syn_FinalPass,q)
+  col = max(col,0.);
+  return vec4(col,depth);
+}
+
+vec4 fpass1() {
+  vec2 
+    q = _uv
   ;
+  
+  vec4
+    col0=texture(pass0,q)
+  , pcol=texture(syn_FinalPass,q)
+  ;
+  
+  vec3 
+    col=col0.xyz
+  ;  
+
   col =tanh(col);
   col= sqrt(col);
 #ifndef KODELIFE
@@ -539,15 +574,19 @@ vec3 effect(vec2 p, vec2 pp, vec2 q) {
   ;
   col=mix(col,mcol.xyz,mcol.w*media_opacity*media_multiplier);
 #endif
-  col=mix(col,pcol.xyz,motion_blur);
-  return col;
+  col=mix(col,pcol.xyz,.25);
+
+  return vec4(col, 1);
 }
 
 vec4 renderMain() {
-  vec2 q = _uv;
-  vec2 p = 2.*_uvc;
-  vec3 col = effect(p, -1.+2.*q,q);
-
-  return vec4(col, 1.0);
+  switch(PASSINDEX) {
+    case 0:
+      return fpass0();
+    case 1:
+      return fpass1();
+    default:
+      return vec4(0);
+  }
 }
 
