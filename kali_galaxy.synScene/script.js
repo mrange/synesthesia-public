0705060c65559e3369560521ebbd0fd76f3f57d0
mrange <marten_range@hotmail.com>
2025-11-28 20:54:50 +0100

    Ditched movement controls

--- kali_galaxy.synScene/script.js
+++ kali_galaxy.synScene/script.js
@@ -19,11 +19,11 @@ var rotationTime      = [0, 0, 0];
 var rotationSpeed     = [0, 0, 0];
 var rotationUniforms  = ["rotation_components_0","rotation_components_1","rotation_components_2"];
 function update(dt) {
-  var hits    = [syn_BassHits,syn_MidHits,syn_MidHighHits];
-  var baseSpeed = [rotation_speed_0.x, rotation_speed_1.x, rotation_speed_2.x];
-  var hitSpeed = [rotation_speed_0.y, rotation_speed_1.y, rotation_speed_2.y];
-  var falloff = [rotation_falloff_0, rotation_falloff_1, rotation_falloff_2];
-  var levels  = [rotation_level_0,rotation_level_1,rotation_level_2];
+  var hits      = [syn_BassHits,syn_MidHits,syn_MidHighHits];
+  var baseSpeed = [0.25,0.25,0.25];
+  var hitSpeed  = [1   ,1   ,1   ];
+  var falloff   = [7   ,7   ,7   ];
+  var levels    = [0.5 ,0.5 ,0.5 ];
   for(var idx=0;idx<3;++idx) {
     if (hits[idx] > 1.41*levels[idx]) {
       rotationSpeed[idx] = hitSpeed[idx];
