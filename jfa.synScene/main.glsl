8700aa748a94ddc08b57adfaa2654b2492e28c8d
mrange <marten_range@hotmail.com>
2025-12-18 22:16:22 +0100

    Tweaks

--- jfa.synScene/main.glsl
+++ jfa.synScene/main.glsl
@@ -1,9 +1,41 @@
 
+const float 
+  lum_cutoff = .1
+ ;
+
 float dot2(vec2 p) {
   return dot(p,p);
 }
 
-vec4 jumpFloodPass(sampler2D tex, vec2 p, vec2 xy, int stp, ivec2 ixy) {
+float texelLum(sampler2D tex, ivec2 ixy) {
+  return dot(vec3(.299, .587, .114),texelFetch(tex,ixy,0).xyz);
+}
+
+vec4 init(sampler2D tex, vec2 xy, ivec2 ixy) {
+   ivec2
+    sz=textureSize(tex, 0)
+  ;
+  vec2
+    seed0=vec2(0)
+  , seed1=vec2(ixy)
+  ;
+  
+
+  ixy += sz/2-ivec2(.5*RENDERSIZE);
+
+  float
+    l=texelLum(tex, ixy)
+  ;
+  
+  if(l>lum_cutoff) {
+    return vec4(seed1, seed0);  
+  } else {
+    return vec4(seed0, seed1);  
+  }
+  
+}
+
+vec4 jfa(sampler2D tex, int stp, vec2 xy, ivec2 ixy) {
   ivec2
     pos
   ;
@@ -62,16 +94,43 @@ vec4 jumpFloodPass(sampler2D tex, vec2 p, vec2 xy, int stp, ivec2 ixy) {
   return vec4(seed0,seed1);
 }
 
+vec4 dist(sampler2D tex, vec2 xy, ivec2 ixy) {
+  vec4
+    t=texelFetch(tex, ixy, 0)
+  ;
+  vec2 
+    seed0=t.xy
+  , seed1=t.zw
+  ;
+
+  float 
+    dist0=distance(xy,seed0)
+  , dist1=distance(xy,seed1)
+  , dist =(dist0>dist1?dist0:-dist1)*2./RENDERSIZE.y
+  , aa   = sqrt(2.)/RENDERSIZE. y
+  ;
+
+  vec3 
+    col=vec3(smoothstep(aa,-aa,abs(dist)-.01))
+  ;
+//  col=vec3(sin(dist*80.));
+  return vec4(col, 1);
+}
 
 vec4 renderMain() {
-  vec2
-    p =_uvc
+  ivec2 
+    ixy=ivec2(_xy)
   ;
-
-  float
-    aa=sqrt(.5)/RENDERSIZE.y
-  , d=length(p)-.5
-  ;
-
-  return vec4(vec3(1)*smoothstep(aa,-aa,d),1);
+  switch(PASSINDEX) {
+  case 0:
+    return init(syn_Media, _xy, ixy);
+  case 1:
+    return jfa(pass0, 4, _xy, ixy);
+  case 2:
+    return jfa(pass1, 2, _xy, ixy);
+  case 3:
+    return jfa(pass1, 1, _xy, ixy);
+  default:
+    return dist(pass2, _xy, ixy);
+  }
 }
